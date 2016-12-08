27d5504077d858e67d974a07c3192829218d6498
swapnil-ss <swapnil31kgp@gmail.com>
2016-12-08 21:55:13 +0530

    Changing into functions

--- testing/wiki_test.py
+++ testing/wiki_test.py
@@ -8,40 +8,47 @@ from nltk.tag import pos_tag
 import enchant
 import string
 
+def correct_words(String_name):
+	tagged_sent = []
+	verbs = [word for word, pos in tagged_sent if pos =='VBZ']
+	# we will check for the correctness if other words apart from nouns or pronouns
+	words = []
+	for verb in verbs :
+		d = enchant.Dict("en_US")
+		if d.check(verb) == True:
+			words.append(verb)
 
-# we will check for the correctness if other words apart from nouns or pronouns
 
-verbs = [word for word, pos in tagged_sent if pos =='VBZ']
+	    if d.check(verb) == False:
+	    	words.append(d.suggest(verb)[0])  #appending the first suggestion from suggested array
 
-words = []
+	return words
 
-for verb in verbs :
-  d = enchant.Dict("en_US")
-  if d.check(verb) == True:
-    words.append(verb)
-  if d.check(verb) == False:
-    words.append(d.suggest(verb)[0])  #appending the first suggestion from suggested array
-
-print words
 
 #now we can look for the second part that is summary of corrected words and nouns
 
 # lets take an example sentence
-sentence = "Barack Obama is president"
+def summary():
+	sentence = "Barack Obama is president"
 #getting the tag of each word after splitting the sentence
-tagged_sent = pos_tag(sentence.split())
+	tagged_sent = pos_tag(sentence.split())
 #assuming we will search only those words which are noun or propernouns
-propernouns = [word for word, pos in tagged_sent if pos =='NNP']
-nouns = [word for word, pos in tagged_sent if pos =='NN']
-propernoun_summary = []
-noun_summary = []
-for propernoun in propernouns:
-  search = wikipedia.search(propernoun)
-  propernoun_summary.append(wikipedia.summary(search[0],sentences=1))
+	propernouns = [word for word, pos in tagged_sent if pos =='NNP']
+	print propernouns
+
+
+	nouns = [word for word, pos in tagged_sent if pos =='NN']
+	print nouns
+	propernoun_summary = []
+	noun_summary = []
+	for propernoun in propernouns:
+  		search = wikipedia.search(propernoun)
+  		propernoun_summary.append(wikipedia.summary(search[0],sentences=1))
   
-for noun in noun:
-  search = wikipedia.search(propernoun)
-  noun_summary.append(wikipedia.summary(search[0],sentences=1))
-print noun_summary
-print propernoun_summary
+	for noun in nouns:
+  		search = wikipedia.search(propernoun)
+  		noun_summary.append(wikipedia.summary(search[0],sentences=1))
+	return "".join(noun_summary)
+	return "".join(propernoun_summary)
+
 
